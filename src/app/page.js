40bdad8605522a4ee5c8f0faffb998fb80dd81da
joesnathan <Jonathanindrawan224@gmail.com>
2026-03-17 16:23:26 +0700

    benerin timenya

--- src/app/page.js
+++ src/app/page.js
@@ -62,6 +62,7 @@ export default function Home() {
         setMatchedCards([]);
         setMoves(0);
         setTime(0);
+        setGameStarted(false);
     }, [difficulty]);
 
     useEffect(() => {
